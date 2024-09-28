4363d40e88ee9cedc34b7228b94b2d32f11a90cc
Mykyta Topchii <nikita.topchiy@gmail.com>
2024-09-28 15:43:48 +0300

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+pub mod app;
+pub mod ui;
+
+use std::io;
+
+use crossterm::event::{self, KeyCode, KeyEventKind};
+use ratatui::{prelude::Backend, style::Stylize, widgets::Paragraph, Terminal};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+fn run<B: Backend>(mut terminal: Terminal<B>) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+                .white()
+                .on_blue();
+            frame.render_widget(greeting, frame.area());
+        })?;
+
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
+}
