1a5ac8392e07ea36249073af9b52b9de5eac10c0
Mykyta Topchii <nikita.topchiy@gmail.com>
2024-10-02 01:32:47 +0300

    Added tabs to delete screens

--- src/ui.rs
+++ src/ui.rs
@@ -4,14 +4,11 @@ use ratatui::{
     layout::{Alignment, Constraint, Layout},
     style::{palette::tailwind, Color, Modifier, Style, Stylize},
     symbols,
-    widgets::{Block, Borders, Clear, List, ListItem, Padding, Paragraph, Tabs, Widget, Wrap},
+    widgets::{Block, Borders, Clear, List, ListItem, Paragraph, Tabs, Wrap},
     Frame,
 };
 
-use crate::{
-    app::{App, AppState},
-    tab::Tab,
-};
+use crate::app::{App, AppState};
 
 pub struct AppUi {}
 
@@ -768,11 +765,25 @@ impl AppUi {
     }
 
     fn render_delete_node_screen(frame: &mut Frame, app: &mut App) {
-        let frame_layout = Layout::new(
+        let titles = app.tabs.iter().map(|t| t.title());
+        let highlight_style = (Color::default(), tailwind::AMBER.c700);
+        let selected_tab_index = app.curr_tab;
+
+        let tabs = Tabs::new(titles)
+            .highlight_style(highlight_style)
+            .select(selected_tab_index);
+
+        let [tabs_rect, work_rect, msg_rect] = Layout::new(
             ratatui::layout::Direction::Vertical,
-            vec![Constraint::Percentage(90), Constraint::Percentage(10)],
+            vec![
+                Constraint::Fill(1),
+                Constraint::Percentage(85),
+                Constraint::Fill(5),
+            ],
         )
-        .split(frame.area());
+        .areas(frame.area());
+
+        frame.render_widget(tabs, tabs_rect);
 
         let data_popup_rect = Layout::new(
             ratatui::layout::Direction::Horizontal,
@@ -791,7 +802,7 @@ impl AppUi {
                     Constraint::Fill(1),
                 ],
             )
-            .split(frame.area())[1],
+            .split(work_rect)[1],
         )[1];
 
         let edit_create_node_path_paragraph = Paragraph::new(app.node_path_buf.as_str())
@@ -806,11 +817,11 @@ impl AppUi {
                     .title_bottom("Esc to cancel | Enter to Delete"),
             );
 
-        let work_layout = Layout::new(
+        let [nodes_list_rect, node_stat_rect] = Layout::new(
             ratatui::layout::Direction::Horizontal,
             vec![Constraint::Fill(3), Constraint::Fill(1)],
         )
-        .split(frame_layout[0]);
+        .areas(work_rect);
 
         let msg_paragraph = Paragraph::new(app.message.as_str()).block(
             Block::default()
@@ -831,12 +842,12 @@ impl AppUi {
         let node_stat = &app.current_node_stat;
         match node_stat {
             Some(_) => {
-                frame.render_widget(Clear, work_layout[1]);
+                frame.render_widget(Clear, node_stat_rect);
                 let stat_list = app.stat_list().block(info_block);
-                frame.render_widget(stat_list, work_layout[1]);
+                frame.render_widget(stat_list, node_stat_rect);
             }
             None => {
-                frame.render_widget(info_block, work_layout[1]);
+                frame.render_widget(info_block, node_stat_rect);
             }
         }
 
@@ -858,17 +869,31 @@ impl AppUi {
             .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
             .highlight_symbol(">>");
 
-        frame.render_stateful_widget(list, work_layout[0], &mut app.list_state);
-        frame.render_widget(msg_paragraph, frame_layout[1]);
+        frame.render_stateful_widget(list, nodes_list_rect, &mut app.list_state);
+        frame.render_widget(msg_paragraph, msg_rect);
         frame.render_widget(edit_create_node_path_paragraph, data_popup_rect);
     }
 
     fn render_confirm_delete_screen(frame: &mut Frame, app: &mut App) {
-        let frame_layout = Layout::new(
+        let titles = app.tabs.iter().map(|t| t.title());
+        let highlight_style = (Color::default(), tailwind::AMBER.c700);
+        let selected_tab_index = app.curr_tab;
+
+        let tabs = Tabs::new(titles)
+            .highlight_style(highlight_style)
+            .select(selected_tab_index);
+
+        let [tabs_rect, work_rect, msg_rect] = Layout::new(
             ratatui::layout::Direction::Vertical,
-            vec![Constraint::Percentage(90), Constraint::Percentage(10)],
+            vec![
+                Constraint::Fill(1),
+                Constraint::Percentage(85),
+                Constraint::Fill(5),
+            ],
         )
-        .split(frame.area());
+        .areas(frame.area());
+
+        frame.render_widget(tabs, tabs_rect);
 
         let data_popup_rect = Layout::new(
             ratatui::layout::Direction::Horizontal,
@@ -887,7 +912,7 @@ impl AppUi {
                     Constraint::Fill(1),
                 ],
             )
-            .split(frame.area())[1],
+            .split(work_rect)[1],
         )[1];
 
         let input_rect = Layout::new(
@@ -903,7 +928,7 @@ impl AppUi {
                 ratatui::layout::Direction::Vertical,
                 vec![
                     Constraint::Fill(10),
-                    Constraint::Fill(3),
+                    Constraint::Fill(4),
                     Constraint::Fill(10),
                 ],
             )
@@ -937,11 +962,11 @@ impl AppUi {
                 .title_bottom("Esc to cancel | Enter to Delete"),
         );
 
-        let work_layout = Layout::new(
+        let [nodes_list_rect, node_stat_rect] = Layout::new(
             ratatui::layout::Direction::Horizontal,
             vec![Constraint::Fill(3), Constraint::Fill(1)],
         )
-        .split(frame_layout[0]);
+        .areas(work_rect);
 
         let msg_paragraph = Paragraph::new(app.message.as_str()).block(
             Block::default()
@@ -962,12 +987,12 @@ impl AppUi {
         let node_stat = &app.current_node_stat;
         match node_stat {
             Some(_) => {
-                frame.render_widget(Clear, work_layout[1]);
+                frame.render_widget(Clear, node_stat_rect);
                 let stat_list = app.stat_list().block(info_block);
-                frame.render_widget(stat_list, work_layout[1]);
+                frame.render_widget(stat_list, node_stat_rect);
             }
             None => {
-                frame.render_widget(info_block, work_layout[1]);
+                frame.render_widget(info_block, node_stat_rect);
             }
         }
 
@@ -989,8 +1014,8 @@ impl AppUi {
             .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
             .highlight_symbol(">>");
 
-        frame.render_stateful_widget(list, work_layout[0], &mut app.list_state);
-        frame.render_widget(msg_paragraph, frame_layout[1]);
+        frame.render_stateful_widget(list, nodes_list_rect, &mut app.list_state);
+        frame.render_widget(msg_paragraph, msg_rect);
         frame.render_widget(edit_create_node_path_paragraph, data_popup_rect);
         frame.render_widget(input_paragraph, input_rect);
     }
